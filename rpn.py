38e097a3dca61ec7d3629a865d1657c94bd368c3
jakehage <jakehage@umich.edu>
2017-03-13 19:36:19 -0400

    Travis final

--- rpn.py
+++ rpn.py
@@ -1,44 +1,42 @@
+
 def calculate(arg):
-	stack = [];
-	result = 0
-	for i in arg:
-		if str(i) == " ":
-			continue
-		if str(i) == "+":
-			sum = 0
-			while stack != []:
-				sum += stack.pop()
-			result = sum
-			print(sum)
-		elif str(i) == "-":
-			difference = 0
-			while stack != []:
-				difference -= stack.pop()
-			result = difference
-			print(difference)
-		elif str(i) == "*":
-			product = 1                  
-			while stack != []:
-				product *= stack.pop()	
-			result = product
-			print(product)
-		elif str(i) == "/":
-			quotient = 0
-			if len(stack) > 1:
-				quotient = stack.pop() / stack.pop()
-				while stack != []:
-					quotient /= stack.pop()
-			elif len(stack) == 1:
-				quotient = stack.pop()
-			else:
-				quotient = 0
-			print(quotient)
-		elif str(i) == "q":
-			exit()
-		else:
-			stack.append(int(i))
+    stack = [];
+    result = 0
+    for i in arg:
+        if str(i) == " ":
+            continue
+        if str(i) == "+":
+            sum = 0
+            while stack != []:
+                sum += stack.pop()
+                result = sum
+            return sum
+        elif str(i) == "-":
+            difference = 0
+            while stack != []:
+                difference -= stack.pop()
+                result = difference
+            return difference
+        elif str(i) == "*":
+            product = 1
+            while stack != []:
+                product *= stack.pop()
+                result = product
+            return product
+        elif str(i) == "/":
+            quotient = 0
+            if stack != []:
+                quotient = stack.pop()
+                while stack != []:
+                    quotient /= stack.pop()
+                    result = quotient
+                return quotient
+        elif str(i) == "q":
+            exit()
+        else:
+            stack.append(int(i))
 def main():
-	while True:
-		calculate(input("rpn calc> "))
+    while True:
+        calculate(input("rpn calc> "))
 if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
-	main()
+    main()
--- test_rpn.py
+++ test_rpn.py
@@ -2,20 +2,18 @@ import unittest
 import rpn
 class TestBasics(unittest.TestCase):
 	def test_add(self):
-		result = rpn.calculate('1 1 +')
-		#print("add result: " + str(result))
-		#print("rpn.calculate(1 1 +): " + str(rpn.calculate("1 1 +")))
-		self.assertEqual(2, int(result))
-		print("Passed add")
+		result = rpn.calculate("1 1 +")
+		self.assertEqual(result, 2)
+		print("Passed add: 1 + 1 = " + str(result))
 	def test_subtract(self):
 		result = rpn.calculate("2 4 -")
 		self.assertEqual(-6, result)
-		print("Passed subtract")
+		print("Passed subtract: 0 - 2 - 4 = " + str(result))
 	def test_multiply(self):
 		result = rpn.calculate("3 5 6 *")
 		self.assertEqual(90, result)
-		print("Passed multiply")
+		print("Passed multiply: 3 * 5 * 6 = " + str(result))
 	def test_divide(self):
 		result = rpn.calculate("4 8 /")
 		self.assertEqual(2, result)
-		print("Passed divide")
+		print("Passed divide: 8 / 4 = " + str(result))
